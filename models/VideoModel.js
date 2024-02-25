6dc536901f7002ab69682cc3c1d9d00cd7b2c14a
CorralesK <kimcorralesv@gmail.com>
2024-02-25 11:53:06 -0600

    Video model created and completed with video and playlist schema

---
+++ models/VideoModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * Scheme for videos
+ */
+const video = new Schema({
+    name: { type: String, required: true },
+    url: { type: String, required: true },
+});
+
+/**
+ * Scheme for playlists
+ */
+const playlist = new Schema({
+    userId: { type: String, required: true },
+    name: { type: String, default: "general" },
+    videos: [{ type: Schema.Types.ObjectId, ref: 'Video' }]
+});
+
+/**
+ * Model for videos
+ */
+const Video = mongoose.model('Video', video);
+
+/**
+ * Model for playlists
+ */
+const Playlist = mongoose.model('Playlist', playlist);
+
+module.exports = { Playlist, Video };
