747bbeba5e5f1b8de344e2c273c934583fe04456
CorralesK <kimcorralesv@gmail.com>
2024-02-25 11:51:17 -0600

    User model created and completed

---
+++ models/UserModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pin: { type: Number, required: true },
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    country: { type: String },
+    dateBirth: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('User', user);
