043ea8fd7caa5e1f979219bc295cc0156735c202
CorralesK <kimcorralesv@gmail.com>
2024-02-26 19:02:10 -0600

    Routes were added and changes were made to the avatar path and the module export.

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,30 +1,47 @@
+const express = require('express');
 const router = express.Router();
 
-export default router;
+module.exports = router;
 
 /**
  * 
  */
-import { UserPost, UserGet } from "../Controllers/UserController.js";
+const {
+    UserPost, 
+    UserGet,
+    UserPinGet
+} = require("../Controllers/UserController.js");
 
 /**
  * Videos
  */
-import { VideoPost, VideoGet, VideoPatch, VideoDelete } from "../Controllers/VideoController.js";
+const {
+    VideoPost, 
+    VideoGet,
+    VideoPatch,
+    VideoDelete
+} = require("../Controllers/VideoController.js");
 
 /**
  * Profiles
  */
-import { ProfilePost, ProfileGet, ProfilePatch, ProfileDelete, AvatarGet } from "../Controllers/ProfileController.js";
-
+const {
+    ProfilePost, 
+    ProfileGet,
+    ProfilePatch,
+    ProfileDelete,
+    AvatarGet,
+    PinGet
+} = require("../Controllers/ProfileController.js");
 
 /**
  * Listen to the task request
  * 
  * Users
  */
-router.get("/users", UserGet);
-router.post("/users", UserPost);
+//router.get("/users", UserGet);
+//router.post("/users", UserPost);
+//router.get("/users/pin", UserPinGet);
 
 /**
  * Videos and Playlist
@@ -44,7 +61,9 @@ router.patch("/profiles", ProfilePatch);
 router.put("/profiles", ProfilePatch);
 router.delete("/profiles", ProfileDelete);
 
+router.get("/profiles/pin", PinGet);
+
 /**
  * Avatars
  */
-router.get("/avatars", AvatarGet);
+router.get("/profiles/avatar", AvatarGet);
