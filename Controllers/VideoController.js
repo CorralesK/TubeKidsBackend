bfa8b3561473190626c319fd51fa78c68b6c9d8d
CorralesK <kimcorralesv@gmail.com>
2024-02-26 18:58:33 -0600

    Changes to optimize code and fix REST code status code.

--- Controllers/VideoController.js
+++ Controllers/VideoController.js
@@ -1,4 +1,4 @@
-const { Playlist, Video } = require("../Models/VideoModel");
+const { Playlist, Video } = require("../Models/VideoModel.js");
 
 /**
  * Method to create a video and add it to the playlist
@@ -7,28 +7,27 @@ const { Playlist, Video } = require("../Models/VideoModel");
  * @param {*} res
  */
 const VideoPost = async (req, res) => {
-    if (req.query && req.query.userId) {
-        const video = new Video();
-
-        video.name = req.body.name;
-        video.url = req.body.url;
-
-        const playlist = PlaylistPatch(video, req.query.userId);
-        if (!playlist) {
-            return res.status(422).json({ error: 'There was an error updating playlist' });
-        }
-
-        await video.save()
-            .then(data => {
-                res.header({ 'location': `/api/videos/?id=${data.id}` });
-                res.status(201).json(data);
-            })
-            .catch(err => {
-                console.error('error while saving the video', err);
-                res.status(422).json({ error: 'There was an error saving the video' });
+    try {
+        if (req.query && req.query.userId) {
+            const video = new Video({
+                name: req.body.name,
+                url: req.body.url
             });
-    } else {
-        res.status(404).json({ error: "User not specified" });
+
+            const playlist = PlaylistPatch(video, req.query.userId);
+            if (!playlist) {
+                return res.status(422).json({ error: 'There was an error updating the playlist' });
+            }
+
+            const savedVideo = await video.save();
+            res.header({ 'location': `/api/videos/?id=${savedVideo.id}` });
+            res.status(201).json(savedVideo);
+        } else {
+            res.status(404).json({ error: "User ID not specified" });
+        }
+    } catch (error) {
+        console.error('Error while saving the video:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
 
@@ -67,35 +66,29 @@ const PlaylistPatch = async (video, userId) => {
  * @param {*} res
  */
 const VideoGet = async (req, res) => {
-    if (req.query && req.query._idid) {
-        await Video.findById(req.query._id)
-            .then((video) => {
-                res.header({
-                    'location': `/api/videos/?id=${video._id}`
-                }).json(video);
-            })
-            .catch(err => {
-                res.status(404).json({ error: err });
-                console.error('error while queryting the video', err);
-            });
-    } else if (req.query.userId) {
-        const playlist = await Playlist.findOne({ userId: req.query.userId });
-        if (!playlist) {
-            return res.status(204).json({ error: 'Playlist not found' });
+    try {
+        // if an specific video is required get it by id
+        if (req.query._id) {
+            const video = await Video.findById(req.query._id);
+            if (!video) {
+                return res.status(404).json({ error: 'Video not found' });
+            }
+            res.header({ 'location': `/api/videos/?id=${video._id}` });
+            res.status(200).json(video);
+        } else if (req.query.userId) {
+            // otherwise get all the videos from a given user
+            const playlist = await Playlist.findOne({ userId: req.query.userId }).populate('videos');
+            if (!playlist) {
+                return res.status(404).json({ error: 'Playlist not found' });
+            }
+            res.header({ 'location': `/api/playlists/?id=${playlist._id}` });
+            res.status(200).json(playlist.videos);
+        } else {
+            return res.status(400).json({ error: 'Invalid request: neither Video ID nor User ID provided' });
         }
-
-        playlist.populate('videos')
-            .then((data) => {
-                res.header({
-                    'location': `/api/videos/?id=${data._id}`
-                }).json(data);
-            })
-            .catch(err => {
-                res.status(404).json({ error: err });
-                console.error('error while queryting the playlist', err);
-            });
-    } else {
-        return res.status(400).json({ error: 'User ID is required' });
+    } catch (error) {
+        console.error('Error while querying videos:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
 
@@ -107,36 +100,31 @@ const VideoGet = async (req, res) => {
  */
 const VideoPatch = async (req, res) => {
     try {
-        if (req.query && req.query._id) {
-            const video = await Video.findById(req.query._id);
-
-            if (!video) {
-                return res.status(404).json({ error: "Video doesn't exist" });
-            }
-
-            video.name = req.body.name;
-            video.url = req.body.url;
-
-            await video.save()
-                .then(data => {
-                    res.status(200).json(data);
-                    res.header({
-                        'location': `/api/videos/?id=${data.id}`
-                    })
-                })
-                .catch(err => {
-                    console.error('Error while saving the video:', err);
-                    res.status(422).json({ error: 'There was an error saving the video' });
-                });
-        } else {
-            res.status(404).json({ error: "Video ID not specified" });
+        if (!req.query || !req.query._id) {
+            return res.status(400).json({ error: "Video ID is required" });
         }
+
+        const video = await Video.findById(req.query._id);
+
+        if (!video) {
+            return res.status(404).json({ error: "Video doesn't exist" });
+        }
+
+        video.name = req.body.name ?? video.name;
+        video.url = req.body.url ?? video.url;
+
+        const updatedVideo = await video.save();
+
+        if (!updatedVideo) {
+            res.status(422).json({ error: 'There was an error saving the video' });
+        }
+
+        res.status(200).json(updatedVideo);
     } catch (error) {
         console.error('Error while updating video:', error);
-        res.status(500).json({ error: error });
+        res.status(500).json({ error: 'There was an error updating the video' });
     }
 }
-
 
 /**
  * Delete a video by its ID.
@@ -145,34 +133,32 @@ const VideoPatch = async (req, res) => {
  * @param {*} res
  */
 const VideoDelete = async (req, res) => {
-    if (req.query && req.query._id) {
+    try {
+        if (!req.query || !req.query._id) {
+            return res.status(400).json({ error: "Video ID is required" });
+        }
 
-        const video = Video.findById(req.query._id);
+        const video = await Video.findById(req.query._id);
 
         if (!video) {
             return res.status(404).json({ error: "Video does not exist" });
         }
 
-        await Playlist.findOneAndUpdate(
+        const playlist = await Playlist.findOneAndUpdate(
             { userId: req.body.userId },
             { $pull: { videos: req.query._id } },
             { new: true }
-        )
-        .catch((err) => {
-            return res.status(404).json({ error: 'Playlist not found' });
-        });
+        );
 
-        video.deleteOne({ _id: req.query._id })
-            .then(() => {
-                res.status(204).json({});
-            })
-            .catch((err) => {
-                console.error('Error while deleting the video:', err);
-                res.status(422).json({ error: 'There was an error deleting the video' });
-            });
-    } else {
-        res.status(404);
-        res.json({ error: "Video does not exist" })
+        if (!playlist) {
+            return res.status(404).json({ error: 'Playlist not found' });
+        }
+
+        await video.deleteOne();
+        res.status(204).json({});
+    } catch (error) {
+        console.error('Error while deleting the video:', error);
+        res.status(422).json({ error: 'There was an error deleting the video' });
     }
 }
 
